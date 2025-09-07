68a2d94dc4727f7549575895d703ea19c51eed42
sufuancse <sufian.cse24@gmail.com>
2025-09-07 19:32:50 +0600

    try to create modal section

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+document.getElementById('all-trees-btn').addEventListener('click', function () {
+    removeActive()
+    const btn = document.getElementById('all-trees-btn')
+    btn.classList.add('active')
+    allPlant()
+})
+
 const allCategory = () => {
     const url = "https://openapi.programming-hero.com/api/categories"
     fetch(url)
@@ -8,11 +15,31 @@ const showCategory = (categories) => {
     const categoryDiv = document.getElementById('all-categories')
     categories.forEach(category => {
         categoryDiv.innerHTML += `
-        <h2 id="${category.id}" class="text-black hover:text-white hover:bg-[#15803D] rounded-sm hover:cursor-pointer p-1 pl-3">${category.category_name}</h2>
+        <h2 id="category-btn-${category.id}" onclick="categoryButton(${category.id})" class="text-black hover:text-white hover:bg-[#15803D] rounded-sm hover:cursor-pointer p-1 pl-3 mb-1 category-btn">${category.category_name}</h2>
         `
         // console.log(category.id);
     });
 }
+
+const removeActive = () => {
+    const categoryBtns = document.querySelectorAll(".category-btn")
+    categoryBtns.forEach(btn => {
+        btn.classList.remove('active')
+    })
+}
+const categoryButton = (id) => {
+    const url = `https://openapi.programming-hero.com/api/category/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            removeActive()
+            const clickBtn = document.getElementById(`category-btn-${id}`)
+            // console.log(clickBtn);
+            clickBtn.classList.add('active')
+            showAllPlant(data.plants)
+        })
+}
+
 
 const allPlant = () => {
     const url = "https://openapi.programming-hero.com/api/plants"
@@ -22,14 +49,15 @@ const allPlant = () => {
 }
 const showAllPlant = (allData) => {
     const card = document.getElementById('card')
+    card.innerHTML = ""
     allData.forEach(data => {
-console.log(data.image);
+        // console.log(data.image);
         card.innerHTML += `
       <div class="bg-white p-4 rounded-xl space-y-2 ">
                         <div class="mx-auto">
                             <img src="${data.image}" class="max-h-[150px] w-full rounded-lg" alt="">
                         </div>
-                        <h1 class="text-xl font-bold hover:cursor-pointer">${data.name}</h1>
+                        <h1  onclick="loadDetails(${data.id})" id=""  class="text-xl font-bold hover:cursor-pointer">${data.name}</h1>
                         <p class="text-sm text-gray-600 line-clamp-3">${data.description}</p>
                         <div class="flex justify-between">
                             <p class=" px-3  py-1 bg-[#DCFCE7] rounded-full text-[#15803D]">${data.category}</p>
@@ -42,8 +70,8 @@ console.log(data.image);
     })
 }
 
-const plantCategory = () => {
-  
+const loadDetails = (id) => {
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`
 }
 
 
@@ -53,7 +81,11 @@ const plantCategory = () => {
 
 
 
+window.onload = () => {
+    document.getElementById("all-trees-btn").classList.add("active")
+    allCategory()
+    allPlant()
+}
 
-
-allCategory()
-allPlant()
+// allCategory()
+// allPlant()
